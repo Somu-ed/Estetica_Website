b38bd86bb26632e475847ec7ae85d3aa24c44b70
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-07-07 00:23:55 +0530

    updates

---
+++ contact_process.php
@@ -0,0 +1,21 @@
+<?php
+include("db_connect.php");
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$contact = $_POST['contact'];
+$address = $_POST['address'];
+$msg = $_POST['msg'];
+
+$query = "INSERT INTO contact(name, email, contact, address, msg) VALUES('$name','$email','$contact','$address','$msg')";
+$fire = mysqli_query($con,$query) or die('Cannot connect to db');
+
+if($fire == true){
+    echo 1;
+}
+else{
+    echo 0;
+}
+
+
+?>
