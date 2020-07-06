34d2fb4cf1992327430718279bf08058bb463592
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-07-06 17:03:00 +0530

    contact added

---
+++ contact.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Contact us | Estetica Furnishing</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Favicon -->
+	<link rel="icon" href="assets/images/favicon.ico">
+
+	<!-- CSS
+	============================================ -->
+	<!-- Bootstrap CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- FontAwesome CSS -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Ionicons CSS -->
+	<link href="assets/css/ionicons.min.css" rel="stylesheet">
+
+	<!-- Themify CSS -->
+	<link href="assets/css/themify-icons.css" rel="stylesheet">
+
+	<!-- Plugins CSS -->
+	<link href="assets/css/plugins.css" rel="stylesheet">
+
+	<!-- Helper CSS -->
+	<link href="assets/css/helper.css" rel="stylesheet">
+
+	<!-- Main CSS -->
+	<link href="assets/css/main.css" rel="stylesheet">
+
+	<!-- Modernizer JS -->
+	<script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+
+<body>
+
+	<!--==== Start of Header offcanvas about ====-->
+    
+    <?php
+    include("header.php");
+    ?>
+
+    <!--===== End of Header offcanvas about ======-->
+
+	<!--=======  breadcrumb area =======-->
+
+	<div class="breadcrumb-area breadcrumb-bg-1 pt-50 pb-70 mb-100">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="breadcrumb-title">Contact Us</h1>
+
+					<!--=======  breadcrumb list  =======-->
+					
+						<ul class="breadcrumb-list">
+							<li class="breadcrumb-list__item"><a href="index.html">HOME</a></li>
+							<li class="breadcrumb-list__item breadcrumb-list__item--active">contact us</li>
+						</ul>
+					
+					<!--=======  End of breadcrumb list  =======-->
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
+    <!--=======  End of breadcrumb area =======-->
+    
+	<!--=============================================
+	=            section title  container      =
+	=============================================-->
+	
+	<div class="section-title-container mb-50">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!--=======  section title  =======-->
+					
+					<div class="section-title section-title--one text-center">
+						<p class="subtitle subtitle--deep">COME HAVE A LOOK</p>
+						<h1>Contact detail</h1>
+					</div>
+					
+					<!--=======  End of section title  =======-->
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of section title container ======-->
+	
+
+	<!--=============================================
+	=            icon box area         =
+	=============================================-->
+	
+	<div class="icon-box-area mb-100 mb-md-30 mb-sm-30">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 mb-md-70 mb-sm-70">
+					<!--=======  single icon box  =======-->
+					
+					<div class="single-icon-box">
+						<div class="icon-box-icon">
+							<i class="ion-location"></i>
+						</div>
+						<div class="icon-box-content">
+							<h3 class="title">ADDRESS</h3>
+							<p class="content">1800 Abbot Kinney Blvd. Unit D & E Venice</p>
+						</div>
+					</div>
+					
+					<!--=======  End of single icon box  =======-->
+				</div>
+				<div class="col-md-4 mb-md-70 mb-sm-70">
+					<!--=======  single icon box  =======-->
+					
+					<div class="single-icon-box mb-10">
+						<div class="icon-box-icon">
+							<i class="ion-ios-telephone"></i>
+						</div>
+						<div class="icon-box-content">
+							<h3 class="title">CONTACT</h3>
+							<p class="content">Mobile: (+88) – 1990 – 6886 <span>Hotline: 1800 – 1102</span></p>
+						</div>
+					</div>
+					
+					<div class="single-icon-box">
+						<div class="icon-box-icon">
+							<i class="ion-ios-email"></i>
+						</div>
+						<div class="icon-box-content">
+							<p class="content">	Mail: contact@lezadastore.com </p>
+						</div>
+					</div>
+					
+					<!--=======  End of single icon box  =======-->
+				</div>
+				<div class="col-md-4 mb-md-70 mb-sm-70">
+					<!--=======  single icon box  =======-->
+					
+					<div class="single-icon-box">
+						<div class="icon-box-icon">
+							<i class="ion-ios-clock-outline"></i>
+						</div>
+						<div class="icon-box-content">
+							<h3 class="title">HOUR OF OPERATION</h3>
+							<p class="content">Monday – Friday : 09:00 – 20:00<span>Sunday & Saturday: 10:30 – 22:00</span></p>
+						</div>
+					</div>
+					
+					<!--=======  End of single icon box  =======-->
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of icon box area  ======-->
+
+	<!--=============================================
+	=            box layout map         =
+	=============================================-->
+	
+	<div class="box-layout-map-area mb-100">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!--=======  box layout map container  =======-->
+					
+					<div class="box-layout-map-container">
+						<div class="google-map" id="google-map-one"></div>
+					</div>
+					
+					<!--=======  End of box layout map container  =======-->
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of box layout map  ======-->
+
+	<!--=============================================
+	=            section title  container      =
+	=============================================-->
+	
+	<div class="section-title-container mb-50">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!--=======  section title  =======-->
+					
+					<div class="section-title section-title--one text-center">
+						<h1>Get in touch</h1>
+					</div>
+					
+					<!--=======  End of section title  =======-->
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of section title container ======-->
+
+
+	<!--=============================================
+	=            contact form area         =
+	=============================================-->
+	
+	<div class="contact-form-area mb-60">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<div class="lezada-form contact-form">
+						<form action="contact.php" method="POST">
+							<div class="row">
+								<div class="col-md-6 mb-40">
+									<input type="text" placeholder="Name *"  name="name"  required>
+								</div>
+								<div class="col-md-6 mb-40">
+									<input type="email" placeholder="Email *"  name="email"  required>
+								</div>
+								<div class="col-lg-12 mb-40">
+									<input type="text" placeholder="Subject"  name="sub" >
+								</div>
+								<div class="col-lg-12 mb-40">
+									<textarea  cols="30" rows="10" placeholder="msg" name="msg" ></textarea>
+								</div>
+								<div class="col-lg-12 text-center">
+									<button name="submit" type="submit" value="submit" class="lezada-button lezada-button--medium">submit</button>
+								</div>
+							</div>
+						</form>
+					</div>
+					<p class="form-messege pt-10 pb-10 mt-10 mb-10"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of contact form area  ======-->
+
+
+	<!--=============================================
+	=            footer area         =
+	=============================================-->
+	
+	<div class="footer-container footer-one pt-100 pb-50">
+		<div class="container wide">
+			<div class="row">
+				<div class="col footer-single-widget">
+					<!--=======  copyright text  =======-->
+						<!--=======  logo  =======-->
+						
+						<div class="logo">
+							<img src="assets/images/logo.png" class="img-fluid" alt="">
+						</div>
+						
+						<!--=======  End of logo  =======-->
+
+						<!--=======  copyright text  =======-->
+						
+						<div class="copyright-text">
+							<p> &copy; 2019 lezada.  <span>All Rights Reserved</span></p>
+						</div>
+						
+						<!--=======  End of copyright text  =======-->
+					
+					<!--=======  End of copyright text  =======-->
+				</div>
+				<div class="col footer-single-widget">
+					<!--=======  single widget  =======-->
+						<h5 class="widget-title">ABOUT</h5>
+
+						<!--=======  footer navigation container  =======-->
+						
+						<div class="footer-nav-container">
+							<nav>
+								<ul>
+									<li><a href="#">About us</a></li>
+									<li><a href="#">Store location</a></li>
+									<li><a href="#">Contact</a></li>
+									<li><a href="#">Orders tracking</a></li>
+								</ul>
+							</nav>
+						</div>
+						
+						<!--=======  End of footer navigation container  =======-->
+					
+					<!--=======  single widget  =======-->
+				</div>
+				<div class="col footer-single-widget">
+					<!--=======  single widget  =======-->
+						<h5 class="widget-title">USEFUL LINKS</h5>
+
+						<!--=======  footer navigation container  =======-->
+						
+						<div class="footer-nav-container">
+							<nav>
+								<ul>
+									<li><a href="#">Returns</a></li>
+									<li><a href="#">Support Policy</a></li>
+									<li><a href="#">Size guide</a></li>
+									<li><a href="#">FAQs</a></li>
+								</ul>
+							</nav>
+						</div>
+						
+						<!--=======  End of footer navigation container  =======-->
+					
+					<!--=======  single widget  =======-->
+				</div>
+
+				<div class="col footer-single-widget">
+					<!--=======  single widget  =======-->
+						<h5 class="widget-title">FOLLOW US ON</h5>
+
+						<!--=======  footer navigation container  =======-->
+						
+						<div class="footer-nav-container footer-social-links">
+							<nav>
+								<ul>
+									<li><a href="//twitter.com"><i class="fa fa-twitter"></i> Twitter</a></li>
+									<li><a href="//facebook.com"> <i class="fa fa-facebook"></i> Facebook</a></li>
+									<li><a href="//instagram.com"><i class="fa fa-instagram"></i> Instagram</a></li>
+									<li><a href="//youtube.com"> <i class="fa fa-youtube"></i> Youtube</a></li>
+								</ul>
+							</nav>
+						</div>
+						
+						<!--=======  End of footer navigation container  =======-->
+						
+					
+					<!--=======  single widget  =======-->
+				</div>
+				<div class="col footer-single-widget">
+					<!--=======  single widget  =======-->
+							
+					<div class="footer-subscription-widget">
+						<h2 class="footer-subscription-title">Subscribe.</h2>
+						<p class="subscription-subtitle">Subscribe to our newsletter to receive news on update.</p>
+
+						<!--=======  subscription form  =======-->
+						
+						<div class="subscription-form">
+							<form id="mc-form" class="mc-form">
+								<input type="email" placeholder="Your email address" required>
+								<button type="submit"><i class="ion-ios-arrow-thin-right"></i></button>
+							</form>
+						</div>
+						
+						<!--=======  End of subscription form  =======-->
+
+						<!-- mailchimp-alerts Start -->
+
+						<div class="mailchimp-alerts">
+							<div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+							<div class="mailchimp-success"></div><!-- mailchimp-success end -->
+							<div class="mailchimp-error"></div><!-- mailchimp-error end -->
+						</div><!-- mailchimp-alerts end -->
+
+					</div>
+					
+					<!--=======  End of single widget  =======-->
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!--=====  End of footer area  ======-->
+
+	<!--=============================================
+	=            overlay items         =
+	=============================================-->
+	
+	<?php include("overlay.php"); ?>
+
+	<!-- scroll to top  -->
+		<a href="#" class="scroll-top"></a>
+	<!-- end of scroll to top -->
+
+	<!-- JS
+	============================================ -->
+	<!-- jQuery JS -->
+	<script src="assets/js/vendor/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="assets/js/popper.min.js"></script>
+
+	<!-- Bootstrap JS -->
+	<script src="assets/js/bootstrap.min.js"></script>
+
+	<!-- Plugins JS -->
+	<script src="assets/js/plugins.js"></script>
+
+	<!-- Main JS -->
+	<script src="assets/js/main.js"></script>
+
+	<!-- AJAX mail JS -->
+	<!-- <script src="assets/js/ajax-mail.js"></script> -->
+
+</body>
+
+</html>
