e3825a501ec54ba04d2374e50a0fe404d8ae3e1c
Somu-ed <satpathysoumyajeet.99@gmail.com>
2020-07-06 17:07:41 +0530

    test mod

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,11 +1,9 @@
 <?php
 
-$db_host="localhos";
+$db_host="localhost";
 $db_username="root";
 $db_password="";
 $db_name="perfect_shades";
-
-
 
 
 $con = mysqli_connect($db_host, $db_username, $db_password, $db_name);
