3fb760298898045427388554cc68fd9e1c7f2728
adityaattrii <adityaattri04.AA@gmail.com>
2020-09-02 20:03:00 +0530

    About Us Page

---
+++ about-us.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us | Estetica Furnishing</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Favicon -->
+	<link rel="icon" href="assets/images/favicon.svg">
+
+	<!-- CSS
+	============================================ -->
+	<!-- Bootstrap CSS -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- FontAwesome CSS -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Ionicons CSS -->
+	<link href="assets/css/ionicons.min.css" rel="stylesheet">
+
+	<!-- Themify CSS -->
+	<link href="assets/css/themify-icons.css" rel="stylesheet">
+
+	<!-- Plugins CSS -->
+	<link href="assets/css/plugins.css" rel="stylesheet">
+
+	<!-- Helper CSS -->
+	<link href="assets/css/helper.css" rel="stylesheet">
+
+	<!-- Main CSS -->
+	<link href="assets/css/main.css" rel="stylesheet">
+
+	<!-- Modernizer JS -->
+	<script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
+
+<body>
+
+	
+	<!--=============================================
+	=            Header offcanvas about         =
+	=============================================-->
+	
+	<?php include "header.php";?>
+	
+	<!--===== End of Header offcanvas about ======-->
+
+	<!--=======  breadcrumb area =======-->
+
+	<div class="breadcrumb-area breadcrumb-bg-2 pt-50 pb-70 mb-100">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="breadcrumb-title">About Us</h1>
+
+					<!--=======  breadcrumb list  =======-->
+					
+						<ul class="breadcrumb-list">
+							<li class="breadcrumb-list__item"><a href="index.html">HOME</a></li>
+							<li class="breadcrumb-list__item breadcrumb-list__item--active">About Us</li>
+						</ul>
+					
+					<!--=======  End of breadcrumb list  =======-->
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
+    <!--=======  End of breadcrumb area =======-->
+    
+
+    <!--=============================================
+	=            section title  container      =
+	=============================================-->
+	
+	<div class="section-title-container mb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <!--=======  section title  =======-->
+                    
+                    <div class="section-title section-title--one text-center">
+                        
+                        <p class="subtitle subtitle--deep">ESTETICA FURNISHING</p>
+                        <h1>Our Story</h1>
+                        <p class="subtitle">Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit met condimentum estibulum is Suspe ndisse suscipit sagittis leo sit met condimentum estibulorem ipsum dolor sit amet, consectetur cing elit.</p>
+                    </div>
+                    
+                    <!--=======  End of section title  =======-->
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!--=====  End of section title container ======-->
+
+    <!--=============================================
+    =            about us video content         =
+    =============================================-->
+    
+    <div class="about-video-content mb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 offset-lg-1">
+                    <!--=======  about video area  =======-->
+                    
+                    <div class="about-video-bg-area about-video-bg-2 pt-300 pb-300 pt-sm-200 pb-sm-200  pt-xs-150 pb-xs-150  mb-50">
+                        <!--=======  floating-text left  =======-->
+                        
+                        <p class="video-text video-text-left"><a href="#">ESTETICA</a></p>
+                        
+                        <!--=======  End of floating-text left  =======-->
+                        <div class="play-icon text-center mb-40">
+                            <a href="https://www.youtube.com/watch?v=feOScd2HdiU" class="popup-video">
+                                <img src="assets/images/icons/icon-play-100x100.png" class="img-fluid" alt="">
+                            </a>
+                        </div>
+                        <h1>OUR STORY</h1>
+
+                        <!--=======  floating-text right  =======-->
+                        
+                        <p class="video-text video-text-right"><a href="#">OUR STORY</a></p>
+                        
+                        <!--=======  End of floating-text right  =======-->
+
+                    </div>
+                    
+                    <!--=======  End of about video area  =======-->
+                </div>
+
+        
+            </div>
+
+            
+        </div>
+    </div>
+    
+    <!--=====  End of about us video content  ======-->
+
+
+    <div class="about-page-content mb-100 mb-sm-45">
+        <div class="container wide">
+            
+            <div class="row">
+
+                <div class="col-lg-6 mb-md-50 mb-sm-50">
+                    <!--=======  about page 2 image  =======-->
+                    
+                    <div class="about-page-2-image">
+                        <img src="assets/images/backgrounds/image-about-us.png" class="img-fluid" alt="">
+                    </div>
+                    
+                    <!--=======  End of about page 2 image  =======-->
+                </div>
+
+                <div class="offset-xl-1 col-xl-4 col-lg-6 col-md-6 mb-sm-50">
+
+                    <div class="about-page-text">
+                        <p class="subtitle subtitle--deep">ESTETICA</p>
+                        <h1>Our Approach</h1>
+                        <p class=" mb-35">Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit estibulum issim Lorem ipsum dolor sit amet, consectetur cing elit. ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit es</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-page-content mb-100 mb-sm-45">
+        <div class="container wide">
+    
+            <div class="row">
+
+                <div class="offset-xl-1 col-xl-4 col-lg-6 col-md-6 mb-sm-50">
+
+                    <div class="about-page-text">
+                        <p class="subtitle subtitle--deep">ESTETICA</p>
+                        <h1>Our Mission</h1>
+                        <p class=" mb-35">Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit estibulum issim Lorem ipsum dolor sit amet, consectetur cing elit. ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit es</p>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-6 mb-md-50 mb-sm-50">
+                    <!--=======  about page 2 image  =======-->
+                    
+                    <div class="about-page-2-image">
+                        <img src="assets/images/backgrounds/image-about-us.png" class="img-fluid" alt="">
+                    </div>
+                    
+                    <!--=======  End of about page 2 image  =======-->
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="about-page-content mb-100 mb-sm-45">
+        <div class="container wide">
+            
+            <div class="row">
+
+                <div class="col-lg-6 mb-md-50 mb-sm-50">
+                    <!--=======  about page 2 image  =======-->
+                    
+                    <div class="about-page-2-image">
+                        <img src="assets/images/backgrounds/image-about-us.png" class="img-fluid" alt="">
+                    </div>
+                    
+                    <!--=======  End of about page 2 image  =======-->
+                </div>
+
+                <div class="offset-xl-1 col-xl-4 col-lg-6 col-md-6 mb-sm-50">
+
+                    <div class="about-page-text">
+                        <p class="subtitle subtitle--deep">ESTETICA</p>
+                        <h1>Our Vision</h1>
+                        <p class=" mb-35">Lorem ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit estibulum issim Lorem ipsum dolor sit amet, consectetur cing elit. ipsum dolor sit amet, consectetur cing elit. Suspe ndisse suscipit sagittis leo sit es</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+	
+
+
+
+	<!--=============================================
+	=            footer area         =
+	=============================================-->
+	
+	<?php include "footer.php"?>
+	
+	<!--=====  End of footer area  ======-->
+
+	<!--=============================================
+	=            overlay items         =
+	=============================================-->
+	
+	<!--=======  about overlay  =======-->
+	
+	<?php include "overlay.php";?>
+	
+	<!--=====  End of overlay items  ======-->
+
+	<!-- scroll to top  -->
+		<a href="#" class="scroll-top"></a>
+	<!-- end of scroll to top -->
+
+	<!-- JS
+	============================================ -->
+	<!-- jQuery JS -->
+	<script src="assets/js/vendor/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="assets/js/popper.min.js"></script>
+
+	<!-- Bootstrap JS -->
+	<script src="assets/js/bootstrap.min.js"></script>
+
+	<!-- Plugins JS -->
+	<script src="assets/js/plugins.js"></script>
+
+	<!-- Main JS -->
+	<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
