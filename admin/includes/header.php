e5a8e7943a227095cbdd9f65cdf370b104328382
NishantRout <nishantnarayanrout@gmail.com>
2020-12-09 00:35:26 +0530

    Header fix

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -10,8 +10,8 @@
             <!--begin::User-->
             <div class="topbar-item">
                 <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-                    <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3"><?php echo("$name"); ?></span>
+                    <span class="text-muted font-weight-bold font-size-base d-md-inline mr-1">Hi,</span>
+                    <span class="text-dark-50 font-weight-bolder font-size-base d-md-inline mr-3"><?php echo("$name"); ?></span>
                     <span class="symbol symbol-35 symbol-light-success">
                         <span class="symbol-label font-size-h5 font-weight-bold"><?php echo("$name[0]"); ?></span>
                     </span>
