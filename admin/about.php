820c2ebbceb491708ce0a2f7f537315409f6d864
NishantRout <nishantnarayanrout@gmail.com>
2021-01-12 17:23:04 +0530

    about meta description update

--- admin/about.php
+++ admin/about.php
@@ -102,7 +102,7 @@ else{
 	<head><base href="">
 		<meta charset="utf-8" />
 		<title>Projects - Compose | Estetica Admin</title>
-		<meta name="description" content="Updates and statistics" />
+		<meta name="description" content="About us Page" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<!--begin::Fonts-->
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
